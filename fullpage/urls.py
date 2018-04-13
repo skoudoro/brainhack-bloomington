21fee157ee842042da476f94ef97f4adba2d596f
Serge Koudoro <skab12@gmail.com>
2018-04-13 21:34:51 +0200

    adding registration url +minor correction

--- fullpage/urls.py
+++ fullpage/urls.py
@@ -3,5 +3,5 @@ from fullpage import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^registration/$', views.registration, name='registration')
+    # url(r'^registration/$', views.registration, name='registration')
 ]
